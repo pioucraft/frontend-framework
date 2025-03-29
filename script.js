7c47c6789a463ca81a13e8fed43f226cf3bcd4a0
Nathan <hello@gougoule.ch>
2025-03-29 15:35:56 +0100

    Refactor dynamic variable handling and update HTML structure with custom elements

--- script.js
+++ script.js
@@ -2,70 +2,100 @@ class dynamicVariable {
     constructor(name, value) {
         this.name = name;
         this.value = value;
-        this.UUID = uuidv4();
-        window.onload = () => {
-            const variables = document.getElementsByTagName("var");
-            Array.from(variables).forEach((variable) => {
-                variable.outerHTML = `<span js="${variable.innerHTML}" class="${
-                    this.UUID
-                } var">${eval(variable.innerHTML)}</span>`;
-            });
 
-            this.updateIfs();
-        };
+        setTimeout(() => {
+            this.updateDisplay();
+        }, 0);
     }
 
-    set(newValue) {
-        this.value = newValue;
+    set(value) {
+        this.value = value;
+
         this.updateDisplay();
     }
 
     updateDisplay() {
-        const displayElement = document.getElementsByClassName("var");
-        if (displayElement.length > 0) {
-            Array.from(displayElement).forEach((element) => {
-                element.innerHTML = this.value;
-            });
-        } else {
-            console.error(
-                "Display elements not found for dynamic variable " + this.name
-            );
-        }
+        this.resetIfs();
+
         this.updateIfs();
+        this.updateJS();
     }
 
-    updateIfs() {
-        const datas = document.querySelectorAll("data[type=if]");
-        for (let i = 0; i < datas.length; i++) {
-            const data = datas[i];
-            this.updateIf(data);
-        }
+    resetIfs() {
+        const elements = document.querySelectorAll(
+            `js-element[update~="${this.name}"][type="if"], js-element[update~="${this.name}"][type="elif"], js-element[update~="${this.name}"][type="else"]`
+        );
+        Array.from(elements).forEach((element) => {
+            element.hidden = true;
+        });
     }
 
-    updateIf(elements) {
-        const existingIfSpans = document.querySelectorAll(`.if-${this.UUID}`);
-        existingIfSpans.forEach((span) => span.remove());
-
-        for (let i = 0; i < elements.children.length; i++) {
-            const element = elements.children[i];
-            if (
-                eval((element.attributes.condition ?? { value: "true" }).value)
-            ) {
-                elements.parentElement.insertAdjacentHTML(
-                    "beforeend",
-                    `<span class="if-${this.UUID}">${element.innerHTML}</span>`
-                );
-                break;
+    updateIfs(from) {
+        if (from) {
+            const nextElement = from.nextElementSibling;
+            if (nextElement && nextElement.attributes.type) {
+                const type = nextElement.attributes.type.value;
+                if (type === "elif") {
+                    const condition = nextElement.attributes.js.value;
+                    if (eval(condition)) {
+                        nextElement.hidden = false;
+                    } else {
+                        nextElement.hidden = true;
+                        this.updateIfs(nextElement);
+                    }
+                } else if (type === "else") {
+                    nextElement.hidden = false;
+                }
             }
+            return;
         }
+
+        const elements = document.querySelectorAll(
+            `js-element[update~="${this.name}"][type="if"]`
+        );
+        Array.from(elements).forEach((element) => {
+            const condition = element.attributes.js.value;
+            if (eval(condition)) {
+                element.hidden = false;
+            } else {
+                element.hidden = true;
+                this.updateIfs(element);
+            }
+        });
+    }
+
+    updateJS() {
+        const elements = document.querySelectorAll(
+            `js-element[update~="${this.name}"]:not([type]), js-element[update~="${this.name}"][type="js"]`
+        );
+        Array.from(elements).forEach((element) => {
+            element.innerHTML = eval(element.attributes.js.value);
+        });
     }
 }
 
-function uuidv4() {
-    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
-        (
-            +c ^
-            (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (+c / 4)))
-        ).toString(16)
-    );
+class jsElement extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    static get observedAttributes() {
+        return ["js", "update", "type"];
+    }
 }
+
+customElements.define("js-element", jsElement);
+
+window.onload = function () {
+    const typeElements = document.querySelectorAll(
+        "js-element[type='if'], js-element[type='elif'], js-element[type='else']"
+    );
+    typeElements.forEach((element) => {
+        element.hidden = true;
+    });
+
+    // Call updateDisplay for all dynamicVariable instances
+    if (window.dynamicVariables) {
+        window.dynamicVariables.forEach((variable) => variable.updateDisplay());
+    }
+};
